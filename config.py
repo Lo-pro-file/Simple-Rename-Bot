f666ac97a4d223caf612bfcda834ecbd0a2091ed
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2022-12-12 21:03:38 +0530

    op

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "7234399"))
+API_HASH = environ.get("API_HASH", "1c521005e55c95e6973ae714d2e9e424")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5666045488:AAFyKF4tvzI0D0xVtPYR3x1dHZHSEqM0KAs")
+ADMIN = int(environ.get("ADMIN", "5684514316"))          
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://graph.org/file/9373d05b762e91dcfe8f5.jpg")
