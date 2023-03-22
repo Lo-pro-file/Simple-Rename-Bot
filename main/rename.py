453b172184a0aae729cd31dd33ad4c65336a78db
Sourcery AI <>
2023-03-22 16:17:16 +0000

    'Refactored by Sourcery'

--- main/rename.py
+++ main/rename.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters, enums
 from config import temp, CAPTION, ADMIN
 from main.utils import progress_message, humanbytes
 
-@Client.on_message(filters.private & filters.command("rename") & filters.user(ADMIN))             
+@Client.on_message(filters.private & filters.command("rename") & filters.user(ADMIN))
 async def rename_file(bot, msg):
     reply = msg.reply_to_message
     if len(msg.command) < 2 or not reply:
@@ -16,8 +16,8 @@ async def rename_file(bot, msg):
     new_name = msg.text.split(" ", 1)[1]
     sts = await msg.reply_text("Trying to Downloading.....")
     c_time = time.time()
-    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
-    filesize = humanbytes(og_media.file_size)                
+    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time))
+    filesize = humanbytes(og_media.file_size)
     if CAPTION:
         try:
             cap = CAPTION.format(file_name=new_name, file_size=filesize)
@@ -26,8 +26,7 @@ async def rename_file(bot, msg):
             return
     else:
         cap = f"{new_name}\n\n💽 size : {filesize}"
-    raw_thumbnail = temp.THUMBNAIL 
-    if raw_thumbnail:
+    if raw_thumbnail := temp.THUMBNAIL:
         og_thumbnail = await bot.download_media(raw_thumbnail)
     else:
         og_thumbnail = await bot.download_media(og_media.thumbs[0].file_id)
@@ -37,7 +36,7 @@ async def rename_file(bot, msg):
         await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
     except Exception as e:  
         await sts.edit(f"Error {e}") 
-        return               
+        return
     try:
         os.remove(downloaded)
         os.remove(og_thumbnail)
